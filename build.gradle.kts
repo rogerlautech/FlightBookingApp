acfa3bf9837c78399b3cf3047eebc6614c2539e9
Ro <roger@4e:ef:e3:58:0b:20.home>
2026-04-04 18:34:07 -0400

    fixed Failure to load Junit Platform by adding junit-platform-launcher

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,8 +20,13 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testCompileOnly("org.projectlombok:lombok")
+    testAnnotationProcessor("org.projectlombok:lombok")
 }
 
 tasks.withType<Test> {
